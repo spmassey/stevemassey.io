efa8b0fa3caf1cab1339dc1b89c8e318a6b2ca6f
Steve Massey <smassey@inspectionsupport.net>
2015-10-23 21:54:58 -0400

    added logging

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ var url = require('url');
 var mongoose = require('mongoose');
 var Q = require('q');
 var flash = require('connect-flash');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 //var passport = require('./')
 
@@ -36,6 +38,22 @@ app.use(session({
 app.set('views', './views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/app'));
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new (winston.transports.File)({ filename: '/var/log/stevemassey.io.log' })
+    ],
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
+    colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
 
 var port = process.env.PORT || 8080;        // set our port
 
