5d40ff1a4f1369b19bd350114af7251cc783bd1b
Steve Massey <smassey@inspectionsupport.net>
2015-10-24 09:28:13 -0400

    morgan for logging

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var server = require('http').createServer();
-var express = require('express');        // call express
+var express = require('express');
 var session = require('express-session');
 var redis = require('redis');
 var RedisClient = redis.createClient();
 var RedisStore = require('connect-redis')(session);
-var app = express();                 // define our app using express
+var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var url = require('url');
 var mongoose = require('mongoose');
-var Q = require('q');
 var flash = require('connect-flash');
-var winston = require('winston');
-var expressWinston = require('express-winston');
+var FileStreamRotator = require('file-stream-rotator');
+var fs = require('fs');
+var morgan = require('morgan');
+var logDirectory = __dirname + '/logs'
 
-//var passport = require('./')
+// ensure the logs dir is good
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
 
-app.locals.moment = require('moment');
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+    filename: logDirectory + '/access-%DATE%.log',
+    frequency: 'daily',
+    verbose: false
+})
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
+
+// app config
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(flash());
@@ -35,27 +38,16 @@ app.use(session({
     }),
     secret: 'Winston_Churchill'
 }));
+
+// jade
 app.set('views', './views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/app'));
 
-app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: true
-        }),
-        new (winston.transports.File)({ filename: '/var/log/stevemassey.io.log' })
-    ],
-    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-    expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
-    colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
-    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-}));
+app.locals.moment = require('moment');
 
-
-var port = process.env.PORT || 8080;        // set our port
+// db
+var port = process.env.PORT || 8080;
 
 var dsn = 'mongodb://localhost/',
     db = 'SteveMasseyIo';
@@ -68,16 +60,11 @@ mongoose.connect(dsn + db, function (err, res) {
     }
 });
 
+// controllers
 app.use(require('./controllers'));
 
-// START THE SERVER
-// =============================================================================
+// listen
 server.on('request', app);
 server.listen(port, function () {
     console.log('Listening on ' + server.address().port)
 });
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/login')
-}
